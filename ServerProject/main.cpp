2584415c221233d68cbed2e28fdbe7332b12522c
kykim9050 <kykim9050@naver.com>
2024-11-20 12:14:40 +0900

    김경윤 : 랜덤 시드 변화를 위해 변수 메모리 주소 활용

--- ServerProject/main.cpp
+++ ServerProject/main.cpp
@@ -31,18 +31,26 @@ int main(void)
 
 	// 그리드 맵 생성
 	std::vector<std::vector<int>> GridMap = std::vector<std::vector<int>>(N, std::vector<int>(N));
+	
+
+	// 임시 랜덤 생성기 (랜덤 시드를 메모리 주소로 활용)
 	// 그리드 내 도착지 Random
 	for (int i = 0; i < N; ++i)
 	{
 		GridMap[0][i] = i + 1;
 	}
-	std::shuffle(GridMap[0].begin(), GridMap[0].end(), std::default_random_engine());
+	int RandA = 0;
+	uint64_t AddressA = reinterpret_cast<uint64_t>(&RandA);
+	std::shuffle(GridMap[0].begin(), GridMap[0].end(), std::default_random_engine(std::mt19937(static_cast<unsigned int>(AddressA))));
 	// 그리드 내 로봇 random생성
 	for (int i = 0; i < N; ++i)
 	{
 		GridMap[N-1][i] = i + 1;
 	}
-	std::shuffle(GridMap[N-1].begin(), GridMap[N-1].end(), std::default_random_engine());
+	int RandB = 0;
+	uint64_t AddressB = reinterpret_cast<uint64_t>(&RandB);
+	std::shuffle(GridMap[N-1].begin(), GridMap[N-1].end(), std::default_random_engine(std::mt19937(static_cast<unsigned int>(AddressB))));
+
 
 	// 화면에 그리드 맵의 상황을 보여줌
 	Display Dis = Display();
