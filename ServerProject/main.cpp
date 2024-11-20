23e1510c169a9258d7c344acd24247b0c646ce2c
kykim9050 <kykim9050@naver.com>
2024-11-20 14:57:48 +0900

    김경윤 : MatchingInfo를 만들어 ID를 Key로 시작좌표, 목적지좌표 매칭 후 저장 구현

--- ServerProject/main.cpp
+++ ServerProject/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "WayPointAlgo.h"
 #include "Point.h"
-
 #include <vector>
+
+// For Test Include
 #include "Display.h"
 #include <algorithm>
 #include <random>
+#include <map>
 
 
 int main(void)
@@ -33,6 +35,7 @@ int main(void)
 	std::vector<std::vector<int>> GridMap = std::vector<std::vector<int>>(N, std::vector<int>(N));
 	
 
+
 	// 임시 랜덤 생성기 (랜덤 시드를 메모리 주소로 활용)
 	// 그리드 내 도착지 Random
 	for (int i = 0; i < N; ++i)
@@ -51,6 +54,19 @@ int main(void)
 	uint64_t AddressB = reinterpret_cast<uint64_t>(&RandB);
 	std::shuffle(GridMap[N-1].begin(), GridMap[N-1].end(), std::default_random_engine(std::mt19937(static_cast<unsigned int>(AddressB))));
 
+	// 목적지와 이동체 1대1 매핑
+	// pair의 first는 출발지 위치, second는 도착지 위치
+	std::map<int, std::pair<Point, Point>> MatchingInfo = std::map<int, std::pair<Point, Point>>();
+	for (int i = 0; i < N; ++i)
+	{
+		MatchingInfo[i + 1] = std::make_pair<Point, Point>(Point(), Point());
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		MatchingInfo[GridMap[N - 1][i]].first = Point(N-1, i);
+		MatchingInfo[GridMap[0][i]].second = Point(0, i);
+	}
+
 
 	// 화면에 그리드 맵의 상황을 보여줌
 	Display Dis = Display();
@@ -58,6 +74,10 @@ int main(void)
 
 	WayPointAlgo NewAlgo = WayPointAlgo(23, Point(4,4), Point(1,1));
 	NewAlgo.FindPath();
-	
+
+
+
+	// 각 로봇마다 경로 출력
+
 	return 0;
 }
