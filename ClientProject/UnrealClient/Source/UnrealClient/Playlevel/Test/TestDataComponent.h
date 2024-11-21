75e1d7e66fb8eac7e6f6f1f0d37efefc914b6a11
kykim9050 <kykim9050@naver.com>
2024-11-21 12:34:32 +0900

    김경윤 : TestDataComponent 초기 구성

--- ClientProject/UnrealClient/Source/UnrealClient/Playlevel/Test/TestDataComponent.h
+++ ClientProject/UnrealClient/Source/UnrealClient/Playlevel/Test/TestDataComponent.h
@@ -7,6 +7,44 @@
 #include "TestDataComponent.generated.h"
 
 
+USTRUCT(BlueprintType)
+struct FCourse
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Test")
+	TArray<FVector2D> CourseArray;
+};
+
+UCLASS(BlueprintType)
+class ATestData : public AActor
+{
+	GENERATED_BODY()
+public:
+
+protected:
+
+private:
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Test", meta = (AllowPrivateAccess = "true"))
+	//TMap<int, FMatchPos> MatchingData;
+
+	/// <summary>
+	/// Key는 이동체의 ID
+	/// Value는 이동체 별 이동 경로 좌표 데이터
+	/// </summary>
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Test", meta = (AllowPrivateAccess = "true"))
+	TMap<int, FCourse> CourseInfo;
+};
+
+USTRUCT(BlueprintType)
+struct FTestDataSet
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Test")
+	TSubclassOf<AActor> TestDataClass = nullptr;
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class UNREALCLIENT_API UTestDataComponent : public UActorComponent
 {
@@ -24,5 +62,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Test", meta = (AllowPrivateAccess = "true"))
+	FTestDataSet TestData;
 };
