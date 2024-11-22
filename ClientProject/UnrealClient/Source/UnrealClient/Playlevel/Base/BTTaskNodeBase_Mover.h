a70fa029cec58c3d77d2a7d6b4f735def6491e4b
kykim9050 <kykim9050@naver.com>
2024-11-22 16:08:09 +0900

    김경윤 : Mover용 Base 테스크노드 추가

---
+++ ClientProject/UnrealClient/Source/UnrealClient/Playlevel/Base/BTTaskNodeBase_Mover.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTaskNodeBase_Mover.generated.h"
+
+/**
+ * 비헤이비어 트리에서 사용할 Mover의 Base 테스크 노드
+ */
+UCLASS()
+class UNREALCLIENT_API UBTTaskNodeBase_Mover : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+
+protected:
+
+private:
+	
+};
