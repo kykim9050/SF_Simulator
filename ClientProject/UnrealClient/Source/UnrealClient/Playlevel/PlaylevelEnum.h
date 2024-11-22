fd8916b6aafd18fc7c4d8199062dd97ae02b0b9c
kykim9050 <kykim9050@naver.com>
2024-11-23 00:12:13 +0900

    김경윤 : Mover의 상태 구별용 EMoverState 추가 및 Mover용 블랙보드에 인자 추가

---
+++ ClientProject/UnrealClient/Source/UnrealClient/Playlevel/PlaylevelEnum.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PlaylevelEnum.generated.h"
+
+/**
+ * Playlevel 내의 Enum 모음
+ */
+
+UENUM(BlueprintType)
+enum class EMoverState : uint8
+{
+	Idle UMETA(Displayname = "Idle"),
+	Accel UMETA(Displayname = "Acceleration"),
+	Decel UMETA(Displayname = "Deceleration"),
+	Rotate UMETA(Displayname = "Rotation"),
+};
