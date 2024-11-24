09baebf07a395ade27b42fa39a316d86c356d71c
kykim9050 <kykim9050@naver.com>
2024-11-24 09:54:29 +0900

    김경윤 : 메인 위젯 업데이트 및 MoverInfoWidget 클래스 추가

---
+++ ClientProject/UnrealClient/Source/UnrealClient/Playlevel/UI/MoverInfoWidget.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Playlevel/UI/MoverInfoWidget.h"
+
---
+++ ClientProject/UnrealClient/Source/UnrealClient/Playlevel/UI/MoverInfoWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Playlevel/Base/ClientPlayBaseUserWidget.h"
+#include "MoverInfoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCLIENT_API UMoverInfoWidget : public UClientPlayBaseUserWidget
+{
+	GENERATED_BODY()
+	
+};
