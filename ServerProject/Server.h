24cf805980929b988292162ce7a26e14dbd854d2
kykim9050 <kykim9050@naver.com>
2024-11-24 21:29:33 +0900

    김경윤 : 소켓 초기화 및 주소 지정

---
+++ ServerProject/Server.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Ό³Έν :
+class Server
+{
+public:
+	// constrcuter destructer
+	Server();
+	~Server();
+
+protected:
+
+private:
+
+};
+
--- ServerProject/main.cpp
+++ ServerProject/main.cpp
@@ -1,13 +1,19 @@
+#pragma comment(lib, "ws2_32.lib")
+#include <WS2tcpip.h>
+#include <WinSock2.h>
 #include <iostream>
 #include "WayPointAlgo.h"
 #include "Point.h"
 #include <vector>
+#include "Server.h"
+#include <assert.h>
 
 // For Test Include
 #include "Display.h"
 #include <algorithm>
 #include <random>
 #include <map>
+
 
 
 int main(void)
@@ -34,6 +40,45 @@ int main(void)
 	// 그리드 맵 생성
 	std::vector<std::vector<int>> GridMap = std::vector<std::vector<int>>(N, std::vector<int>(N));
 	
+
+	// ---------------------------------
+	// 서버 열기
+	std::cout << "Server Open" << std::endl;
+	Server MainServer = Server();
+
+	//// 소켓 초기화
+	// 윈도우로부터 소켓을 사용하기 위해 활용하는 구조체 WSData
+	WSAData WsaData = WSAData();
+	// 윈도우의 소켓을 초기화 하는 함수
+	int Error = WSAStartup(MAKEWORD(2, 2), &WsaData);
+	if (SOCKET_ERROR == Error)
+	{
+		// 반환값이 에러에 해당되면 치명적인 오류 발생 (소켓 초기화 오류)
+		// 인자가 false에 해당하면 assert 활성화
+		assert(false);
+	}
+
+	//// 전송 주소, 포트 지정
+	int Port = 30000;
+	SOCKADDR_IN Address = {};
+	Address.sin_family = AF_INET;
+	Address.sin_port = htons(Port);
+
+	// 네트워크 주소를 인자로 받아 이진수로 출력
+	// 서버라서 임의 주소 지정
+	if (SOCKET_ERROR == inet_pton(AF_INET, "0.0.0.0", &Address.sin_addr))
+	{
+		assert(false);
+	}
+
+	// -----------------------------
+
+
+
+
+
+
+
 
 
 	// 임시 랜덤 생성기 (랜덤 시드를 메모리 주소로 활용)
